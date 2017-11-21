d5b0003d73ca6757e35978067f4898a34e49ec75
Ben Kautt <bkautt73@gmail.com>
2017-11-20 19:23:09 -0800

    added init spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,16 @@
-# Your code here
+require "calculator"
+
+describe Calculator do
+  let(:calc){Calculator.new}
+
+  describe '#initialize' do
+
+    it 'should give you a calculator' do
+      expect(calc).to be_a(Calculator)
+    end
+
+    it 'shouldn\'t have anything in memory' do
+      expect(calc.memory).to be_nil
+    end
+  end
+end
