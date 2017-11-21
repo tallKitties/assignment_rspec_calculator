19d809e53134eeec85ee66929c9ae92bcfba4796
Ben Kautt <bkautt73@gmail.com>
2017-11-20 19:32:59 -0800

    created #add spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,4 +13,21 @@ describe Calculator do
       expect(calc.memory).to be_nil
     end
   end
+
+  describe '#add' do
+
+    it "should add 2 positive integers" do
+      expect(calc.add(1,2)).to eq(3)
+    end
+
+    it "should add 2 positive floats" do
+      expect(calc.add(1.5,2)).to eq(3.5)
+    end
+
+    it "should handle negatives correctly" do
+      expect(calc.add(1,-2)).to eq(-1)
+      expect(calc.add(-1,2)).to eq(1)
+      expect(calc.add(1.5,-2)).to eq(-0.5)
+    end
+  end
 end
