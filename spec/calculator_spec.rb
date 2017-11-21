d13ef53699c9e96d16a4d8df600e0d061ade6456
Ben Kautt <bkautt73@gmail.com>
2017-11-20 19:44:17 -0800

    created #subtract spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -27,7 +27,23 @@ describe Calculator do
     it "should handle negatives correctly" do
       expect(calc.add(1,-2)).to eq(-1)
       expect(calc.add(-1,2)).to eq(1)
-      expect(calc.add(1.5,-2)).to eq(-0.5)
+      expect(calc.add(1.5,-2.3)).to be_within(0.1).of(-0.8)
+    end
+  end
+
+  describe '#subtract' do
+    
+    it "should subtract 2 positive integers" do
+      expect(calc.subtract(2,1)).to eq(1)
+    end
+
+    it "should subtract 2 positive floats" do
+      expect(calc.subtract(2.5,1.3)).to be_within(0.1).of(1.2)
+    end
+
+    it "should handle negatives correctly" do
+      expect(calc.subtract(-1,-2)).to eq(1)
+      expect(calc.add(-1.5,-2.3)).to be_within(0.1).of(-3.8)
     end
   end
 end
